b19fa9b5376e8ba051ce6a4fe0a3fd0491917593
hallie214 <younh524@newschool.edu>
2022-10-06 23:47:22 -0400

    Portfolio

---
+++ portfolio1.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+
+// alert("[whatever-little-message-you-want]");
+
+// $("#titlename").hover(function(){
+// $("#titlename").css("color", "grey");
+// });
+// $("#titlename").mouseout(function(){
+// $("#titlename").css("color", "black");
+// });
+
+// $("#coverlogo").click(function(){
+// $("#cover").hide();
+// });
+// $("#coverlogo").click(function(){
+// $("#coverlogo").hide();
+// });
+
+$("#header2").hover(function(){
+$("#header2").css("color", "grey");
+});
+$("#header2").mouseout(function(){
+$("#header2").css("color", "black");
+});
+$("#header3").hover(function(){
+$("#header3").css("color", "grey");
+});
+$("#header3").mouseout(function(){
+$("#header3").css("color", "black");
+});
+
+$("#contact1").hover(function(){
+$("#contact1").css("color", "grey");
+});
+$("#contact1").mouseout(function(){
+$("#contact1").css("color", "black");
+});
+$("#contact2").hover(function(){
+$("#contact2").css("color", "grey");
+});
+$("#contact2").mouseout(function(){
+$("#contact2").css("color", "black");
+});
+$("#contact3").hover(function(){
+$("#contact3").css("color", "grey");
+});
+$("#contact3").mouseout(function(){
+$("#contact3").css("color", "black");
+});
+
+// $("#[the-id-of-the-item-you-element-to-drag]").draggable();
+//
+// $("#[the-id-of-element-b]").hover(function(){
+// $("#[the-id-of-element-a]").show();
+// });
+//
+// $("#[the-id-of-what-you'll-hover-on]").hover(function(){
+// $("#[the-id-of-the-element-you-want-to-have-different-css]").css("[the-new-property]", "[the-new-value]");
+// });
+// $("#[the-id-of-what-you'll-hover-on]").mouseout(function(){
+// $("#[the-id-of-the-element-you-want-to-have-different-css]").css("[the-new-property]", "[the-new-value]");
+// });
+
+})
---
+++ portfolio3/portfolio3.js
@@ -0,0 +1,176 @@
+$(document).ready(function() {
+
+// alert("[whatever-little-message-you-want]");
+
+$("#header1").hover(function(){
+$("#header1").css("color", "grey");
+});
+$("#header1").mouseout(function(){
+$("#header1").css("color", "black");
+});
+$("#header2").hover(function(){
+$("#header2").css("color", "grey");
+});
+$("#header2").mouseout(function(){
+$("#header2").css("color", "black");
+});
+
+$("#name1").hover(function(){
+$("#name1").css("color", "grey");
+});
+$("#name1").mouseout(function(){
+$("#name1").css("color", "black");
+});
+$("#name2").hover(function(){
+$("#name2").css("color", "grey");
+});
+$("#name2").mouseout(function(){
+$("#name2").css("color", "black");
+});
+$("#name3").hover(function(){
+$("#name3").css("color", "grey");
+});
+$("#name3").mouseout(function(){
+$("#name3").css("color", "black");
+});
+$("#name4").hover(function(){
+$("#name4").css("color", "grey");
+});
+$("#name4").mouseout(function(){
+$("#name4").css("color", "black");
+});
+$("#name5").hover(function(){
+$("#name5").css("color", "grey");
+});
+$("#name5").mouseout(function(){
+$("#name5").css("color", "black");
+});
+$("#name6one").hover(function(){
+$("#name6one").css("color", "grey");
+});
+$("#name6one").mouseout(function(){
+$("#name6one").css("color", "black");
+});
+$("#name6two").hover(function(){
+$("#name6two").css("color", "grey");
+});
+$("#name6two").mouseout(function(){
+$("#name6two").css("color", "black");
+});
+
+$("#contact1").hover(function(){
+$("#contact1").css("color", "grey");
+});
+$("#contact1").mouseout(function(){
+$("#contact1").css("color", "black");
+});
+$("#contact2").hover(function(){
+$("#contact2").css("color", "grey");
+});
+$("#contact2").mouseout(function(){
+$("#contact2").css("color", "black");
+});
+$("#contact3").hover(function(){
+$("#contact3").css("color", "grey");
+});
+$("#contact3").mouseout(function(){
+$("#contact3").css("color", "black");
+});
+
+$("#name1").hover(function(){
+$("#preview1").show();
+});
+$("#name1").mouseout(function(){
+$("#preview1").hide();
+});
+$("#name1").hover(function(){
+$("#preview2").show();
+});
+$("#name1").mouseout(function(){
+$("#preview2").hide();
+});
+$("#name1").hover(function(){
+$("#title").hide();
+});
+$("#name1").mouseout(function(){
+$("#title").show();
+});
+
+$("#name2").hover(function(){
+$("#preview3").show();
+});
+$("#name2").mouseout(function(){
+$("#preview3").hide();
+});
+$("#name2").hover(function(){
+$("#preview4").show();
+});
+$("#name2").mouseout(function(){
+$("#preview4").hide();
+});
+$("#name2").hover(function(){
+$("#title").hide();
+});
+$("#name2").mouseout(function(){
+$("#title").show();
+});
+
+$("#name3").hover(function(){
+$("#preview5").show();
+});
+$("#name3").mouseout(function(){
+$("#preview5").hide();
+});
+$("#name3").hover(function(){
+$("#preview6").show();
+});
+$("#name3").mouseout(function(){
+$("#preview6").hide();
+});
+$("#name3").hover(function(){
+$("#title").hide();
+});
+$("#name3").mouseout(function(){
+$("#title").show();
+});
+
+$("#name4").hover(function(){
+$("#preview7").show();
+});
+$("#name4").mouseout(function(){
+$("#preview7").hide();
+});
+$("#name4").hover(function(){
+$("#preview8").show();
+});
+$("#name4").mouseout(function(){
+$("#preview8").hide();
+});
+$("#name4").hover(function(){
+$("#title").hide();
+});
+$("#name4").mouseout(function(){
+$("#title").show();
+});
+
+// $("#name6").hover(function(){
+// $("#name6").css("color", "grey");
+// });
+// $("#name6").mouseout(function(){
+// $("#name6").css("color", "black");
+// });
+//
+// $("#[the-id-of-the-item-you-element-to-drag]").draggable();
+//
+// $("#[the-id-of-element-b]").hover(function(){
+// $("#[the-id-of-element-a]").show();
+// });
+//
+// $("#[the-id-of-what-you'll-hover-on]").hover(function(){
+// $("#[the-id-of-the-element-you-want-to-have-different-css]").css("[the-new-property]", "[the-new-value]");
+// });
+// $("#[the-id-of-what-you'll-hover-on]").mouseout(function(){
+// $("#[the-id-of-the-element-you-want-to-have-different-css]").css("[the-new-property]", "[the-new-value]");
+// });
+
+})
